d92d289c64431664ebc11a00ac23a02ebce56884
EunHyeJung <ehye826@gmail.com>
2018-08-10 01:22:34 +0900

    일반회원가입 기능 구현

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth import logout
 urlpatterns = [
     path('', include('social_django.urls', namespace='social')),
     #path('logout/', logout, {'next_page': settings.LOGOUT_REDIRECT_URL},  name='logout'),
-    path('logout/', views.logout, name='logout')
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup')
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def logout(request):
     auth.logout(request)
     return redirect('/')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth.login(request, user, backend ='django.contrib.auth.backends.ModelBackend')
+            return redirect('/posts/list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'authentication/signup.html', {'form' : form})
